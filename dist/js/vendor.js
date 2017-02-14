7b229541e6237d93947dca9afa9931b9dc945a0a
Natalia Chigryay <spiteful.liar@gmail.com>
2017-02-14 16:30:49 +0300

    initial commit (quickstart clone)

---
+++ dist/js/vendor.js
@@ -0,0 +1,11 @@
+//vendor libs
+require('jquery');
+import slick from 'slick-carousel';
+// require('async');
+// require('moment');
+
+// vendor-styles
+require('normalize.css/normalize.css');
+require('angular/angular-csp.css');
+// require('angular-material/angular-material.css');
+
